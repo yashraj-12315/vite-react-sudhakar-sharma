6f7518a1d37942ec1647b721d5446a9b451958f0
Yash Raj <yashraj4586@gmail.com>
2025-10-22 00:18:15 +0530

    Create Manual Validation using Formik

--- src/components/form-event/formik.jsx
+++ src/components/form-event/formik.jsx
@@ -1,12 +1,61 @@
 import { useFormik } from "formik";
 export function Formik() {
+  function ValidateUser(formData) {
+    var errors = {
+      UserName: "",
+      Email: "",
+      Age: "",
+      Gender: "",
+      City: "",
+      Mobile: "",
+    };
+    if (formData.UserName.length === 0) {
+      errors.UserName = "User Name is required";
+    } else {
+      if (formData.UserName.length < 5) {
+        errors.UserName = "User Name should be minimum 5 characters";
+      }
+    }
+
+    if (formData.Age.length === 0) {
+      errors.Age = "Age is required";
+    } else {
+      if (isNaN(formData.Age)) {
+        errors.Age = "Age should be numeric value";
+      } else {
+        errors.Age = "";
+      }
+    }
+
+    if (formData.Gender === "") {
+      errors.Gender = "Please select Gender";
+    } else {
+      errors.Gender = "";
+    }
+
+    if (formData.City === "") {
+      errors.City = "Please select City";
+    } else {
+      errors.City = "";
+    }
+
+    if (formData.Mobile.match(/\+91\d{10}/)) {
+      errors.Mobile = "";
+    } else {
+      errors.Mobile = "Mobile number should be in +91xxxxxxxxxx format";
+    }
+    return errors;
+  }
   const formik = useFormik({
     initialValues: {
       UserName: "",
       Email: "",
       Age: 0,
       Gender: "",
+      City: "",
+      Mobile: "",
     },
+    validate: ValidateUser,
     onSubmit: (data) => {
       console.log(data);
     },
@@ -25,14 +74,19 @@ export function Formik() {
           <dd>
             <input type="text" name="UserName" onChange={formik.handleChange} />
           </dd>
+          <dd className="text-danger">{formik.errors.UserName}</dd>
           <dt>Email</dt>
           <dd>
             <input type="email" name="Email" onChange={formik.handleChange} />
           </dd>
+          <dd className="text-danger">{formik.errors.Email}</dd>
+
           <dt>Age</dt>
           <dd>
             <input type="number" name="Age" onChange={formik.handleChange} />
           </dd>
+          <dd className="text-danger">{formik.errors.Age}</dd>
+
           <dt>Gender</dt>
           <dd>
             <input
@@ -52,6 +106,23 @@ export function Formik() {
             />
             <label htmlFor="female">Female</label>
           </dd>
+          <dd className="text-danger">{formik.errors.Gender}</dd>
+
+          <dt>City</dt>
+          <dd>
+            <select name="City" onChange={formik.handleChange}>
+              <option value="">Select City</option>
+              <option value="Delhi">Delhi</option>
+              <option value="Hyderabad">Hyderabad</option>
+            </select>
+          </dd>
+          <dd className="text-danger">{formik.errors.City}</dd>
+
+          <dt>Mobile</dt>
+          <dd>
+            <input type="text" name="Mobile" onChange={formik.handleChange} />
+          </dd>
+          <dd className="text-danger">{formik.errors.Mobile}</dd>
         </dl>
         <button type="submit">Submit</button>
       </form>
